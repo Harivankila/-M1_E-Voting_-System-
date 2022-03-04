8288b573f210b0a3f041cfa8e7387d9300fc2c88
Harivankila <98792351+Harivankila@users.noreply.github.com>
2022-03-04 10:41:42 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// #include"election.h"
 // define macroname charseq
-// total no of the contestants
+// total no of the parties
 #define total_no_of_parties
-#define party_no_1 "Honda"
-#define party_no_2 "Yamaha" 
-#define party_no_3 "Ducati" 
-#define party_no_4 "KTM"
+#define party_no_1 "Candidate Rahul"
+#define party_no_2 "Candidate Dev" 
+#define party_no_3 "Candidate Kumar" 
+#define party_no_4 "Candidate Hari"
 
-int vote_Count1 = 0,
-    vote_Count2 = 0,
-    vote_Count3 = 0,
-    vote_Count4 = 0,
+int vc1 = 0,
+    vc2 = 0,
+    vc3 = 0,
+    vc4 = 0,
     invalid_votes = 0;
 
 void castVote(){
 
-    int choice;
-    printf("\n \n <#> <#> <#> <#> Please chooose your Candidate <#> <#> <#> <#> \n \n");
+    int c;
+    printf("\n \n ** Please chooose your Candidate **** \n \n");
     printf("\n 1. %s", party_no_1);
     printf("\n 2. %s", party_no_2);
     printf("\n 3. %s", party_no_3);
@@ -27,20 +26,20 @@ void castVote(){
     printf("\n 5. %s", "None of the above");
 
     printf("\n \n Input your choice ( 1 - 4 ) : ");
-    scanf("%d", &choice);
+    scanf("%d", &c);
 
-    switch (choice){
+    switch (c){
         case 1:
-        vote_Count1++;
+        vc1++;
         break;
         case 2:
-        vote_Count2++;
+        vc2++;
         break;
         case 3:
-        vote_Count3++;
+        vc3++;
         break;
-        case 4:
-        vote_Count4++;
+         case 4:
+        vc4++;
         break;
         case 5:
         invalid_votes++;
@@ -55,43 +54,42 @@ void castVote(){
 }
 
 void votesCount(){
-    printf("\n \n #_#_#_#_# Voting Statics #_#_#_#_#");
-    printf("\n %s - %d", party_no_1, vote_Count1);
-    printf("\n %s - %d", party_no_2, vote_Count2);
-    printf("\n %s - %d", party_no_3, vote_Count3);
-    printf("\n %s - %d", party_no_4, vote_Count4);
+    printf("\n \n ##### Voting Statics #####");
+    printf("\n %s - %d", party_no_1, vc1);
+    printf("\n %s - %d", party_no_2, vc2);
+    printf("\n %s - %d", party_no_3, vc3);
+    printf("\n %s - %d", party_no_4, vc4);
     printf("\n %s - %d", "Spoiled Votes", invalid_votes);
 
 }
 
 void getLeadingCandiate(){
-    printf("\n \n #_#_#_#_# Leading Candiate #_#_#_#_#");
-    if(vote_Count1 > vote_Count2 && vote_Count1 > vote_Count3 && vote_Count1 > vote_Count4){
+    printf("\n \n ##### Leading Candiate #####");
+    if(vc1 > vc2 && vc1 > vc3 && vc1 > vc4){
         printf("The Leading Candiate is %s", party_no_1);
-    }else if(vote_Count2 > vote_Count1 && vote_Count2 > vote_Count3 && vote_Count2 > vote_Count4){
+    }else if(vc2 > vc1 && vc2 > vc3 && vc2 > vc4){
         printf("The Leading Candiate is %s", party_no_2);
-    }else if(vote_Count3 > vote_Count2 && vote_Count3 > vote_Count1 && vote_Count3 > vote_Count4){
+    }else if(vc3 > vc2 && vc3 > vc1 && vc3 > vc4){
         printf("The Leading Candiate is %s", party_no_3);
-    }else if(vote_Count4 > vote_Count2 && vote_Count4> vote_Count3 && vote_Count4 > vote_Count1){
+    }else if(vc4 > vc2 && vc4> vc3 && vc4 > vc1){
         printf("The Leading Candiate is %s", party_no_4);
     }else {
         printf("+++++++++++++ Warning!!!!!!!!! No Win-Win Situation ++++++++++++++");
     }
 }
-
 int main(){
-    int choice;
+    int ch;
     do{
-        printf("\n \n #_#_#_#_#_# WELCOME TO ELECTION / VOTING 2022 A.D #_#_#_#_#_#");
-        printf("\n \n 1. Cast the Vote.");
-        printf("\n \n 2. Find the Vote Count.");
-        printf("\n \n 3. Find the Leading Candidate.");
+        printf("\n \n *** WELCOME TO ELECTION / VOTING 2022 A.D ***");
+        printf("\n \n 1. Cast  Your Vote.");
+        printf("\n \n 2. Find your Vote Count.");
+        printf("\n \n 3. Find your Leading Candidate.");
         printf("\n \n 0. Exit.");
 
         printf("\n \n Please Enter Your Choice : ");
-        scanf("%d", &choice);
+        scanf("%d", &ch);
 
-        switch(choice){
+        switch(ch){
         case 1:
         castVote();
         break;
@@ -102,10 +100,11 @@ int main(){
         getLeadingCandiate();
         break;
         default:
-        printf("\n Error: Invalid Choice !!!");
+        printf("\n Error: Invalid Choice PLease cast your vote again!!!");
         }
 
-    } while (choice!=0);
+    } while (ch!=0);
 
 getchar();
 }
+         
